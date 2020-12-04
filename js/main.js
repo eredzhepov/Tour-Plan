80bb1130f6256f65e574d8aa19d580c16064c19d
eredzhepov <en.eredzhep@mail.ru>
2020-12-04 16:11:57 +0400

    Create modal window

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+var menuButton = document.querySelector(".menu-button")
+menuButton.addEventListener('click', function(){
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
+})
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -24,5 +30,36 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   },
 
 
-})
+});
 
+var modalButton = $("[data-togle=modal]");
+var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+
+  function openModal() {
+    var  modalOverlay = $(".modal__overlay");
+    var  modalDialog = $(".modal__dialog");
+    var  modalDialog = $(".modal__dialog");
+  
+  modalOverlay.addClass("modal__overlay--visible");
+  modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault()
+    var  modalOverlay = $(".modal__overlay");
+    var  modalDialog = $(".modal__dialog");
+    var  modalDialog = $(".modal__dialog");
+  
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+  }
+
+$(document).keydown(function(event) { 
+  if (event.keyCode === 27) { 
+    closeModal(event);
+  }
+});
+
+});
---
+++ send.php
@@ -0,0 +1,61 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
+// Формирование самого письма
+$title = "New message Best Tour Plan";
+$body = "
+<h2>New message</h2>
+<b>Name:</b> $name<br>
+<b>Phone:</b> $phone<br><br>
+<b>Email:</b> $email<br><br>
+<b>Message:</b><br>$message
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'en.eredzhep@mail.ru'; // Логин на почте
+    $mail->Password   = '79780253621Enver'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('en.eredzhep@mail.ru', 'En Eredzhep'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('eredzhepov.enver@yandex.ua');  
+   
+
+    
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
